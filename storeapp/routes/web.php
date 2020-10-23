d7f8d19fbf4a76e3bf42c56e6d1f9e0052e6a482
Loc <tanlocit9@gmail.com>
2020-10-23 13:01:39 +0700

    update web.php

--- storeapp/routes/web.php
+++ storeapp/routes/web.php
@@ -16,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 //Route::get('category/{slug}', 'Frontend\CategoryController@index')->name('category');
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('backend/layouts/dashboard');
+})->name('dashboard');
+
+Route::get('/content', function () {
+    return view('backend/template');
+})->name('template');
